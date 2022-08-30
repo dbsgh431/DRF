fb8aab7286fc5ef3d0bddd35a4fb2bbf5656b9af
yoonho <qufdbsgh431@gmail.com>
2022-08-30 16:09:52 +0900

    function based view api, serializer 정의

---
+++ myweb/example/models.py
@@ -0,0 +1,13 @@
+
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    bid = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=30)
+    author = models.CharField(max_length=30)
+    catecory = models.CharField(max_length=30)
+    pages = models.IntegerField()
+    price = models.IntegerField()
+    published_date = models.DateField()
+    description = models.TextField()
---
+++ myweb/example/urls.py
@@ -0,0 +1,7 @@
+from .views import HelloAPI
+from django.urls import path,include
+
+
+urlpatterns = [
+    path('hello/', HelloAPI),
+]
