c6e6d286bfeee86d863c53f593bb07eb5f63fa00
yoonho <qufdbsgh431@gmail.com>
2022-09-05 17:57:19 +0900

    Todo 앱 생성, 모델, 시리얼라이저 정의

---
+++ myweb/todo/models.py
@@ -0,0 +1,14 @@
+from turtle import title
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    important = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+
+
+    def __str__(self) -> str:
+        return self.title
